f0f5f7805bb58ca7cac061be7ac3114c2e843c82
viale.valerio@gmail.com <viale.valerio@gmail.com>
2023-10-24 09:11:02 +0200

    heat map

--- notebook.py
+++ notebook.py
@@ -4,14 +4,25 @@
 
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Input data files are available in the read-only "../input/" directory
 # For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
 
 import os
-for dirname, _, filenames in os.walk('/'):
+for dirname, _, filenames in os.walk('/kaggle/input'):
     for filename in filenames:
         print(os.path.join(dirname, filename))
+
+titanic_data=pd.read_csv('train.csv')
+titanic_data.describe()
+subset_data = titanic_data[['PassengerId','Age', 'Fare', 'Pclass', 'Survived','SibSp', 'Parch']]
+sns.heatmap(subset_data.corr(), cmap="YlGnBu")
+plt.show()        
+
+# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
+# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
 
 # You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
 # You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
